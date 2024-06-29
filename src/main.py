1581d3ee66c87aefc217165a4902c3b294d16a03
Michael Ching <chingdaotze@yahoo.com>
2024-06-29 16:42:23 -0400

    Added exception handling.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import (
+    showinfo,
+    showerror,
+)
+from traceback import format_exc
 
 from heif_to_jpg.get_directory import get_directory
 from heif_to_jpg.convert_directory import convert_directory
@@ -6,33 +10,38 @@ from heif_to_jpg.convert_directory import convert_directory
 
 def main() -> None:
 
-    print(
-        "Starting app...",
-    )
+    try:
+        print(
+            "Starting app...",
+        )
 
-    # Get directories
-    heif_dir = get_directory(
-        file_dialog_title="*.heif Input Directory",
-    )
+        # Get directories
+        heif_dir = get_directory(
+            file_dialog_title="*.heif Input Directory",
+        )
 
-    jpeg_dir = get_directory(
-        file_dialog_title="*.jpeg Output Directory",
-    )
+        jpeg_dir = get_directory(
+            file_dialog_title="*.jpeg Output Directory",
+        )
 
-    # Convert directory
-    convert_directory(
-        source_dir=heif_dir,
-        target_dir=jpeg_dir,
-    )
+        # Convert directory
+        convert_directory(
+            source_dir=heif_dir,
+            target_dir=jpeg_dir,
+        )
 
-    print(
-        "Done!",
-    )
+        print(
+            "Done!",
+        )
 
-    showinfo(
-        title="Success",
-        message="Done!",
-    )
+        showinfo(
+            title="Success",
+            message="Done!",
+        )
+
+    except Exception as e:
+        showerror(title="Exception", message=format_exc())
+        raise e
 
 
 if __name__ == "__main__":
